42adcf3a851cb347c3222a2d0e851bb088a9868e
Kiran Adhikari <kiran.adhikari@innovatetech.co>
2021-07-27 17:38:29 +0545

    Fundamental WIP

--- api/controllers/fundamental.go
+++ api/controllers/fundamental.go
@@ -10,26 +10,28 @@ import (
 )
 
 type KeyFinancialMetrics struct {
-	Ticker                     string                  `json:"ticker"`
-	LTP                        float64                 `json:"ltp"`
-	DiversionFromFair          float64                 `json:"divesionFromFair"`
-	PE                         float64                 `json:"pe"`
-	Eps                        float64                 `json:"eps"`
-	FairValue                  float64                 `json:"fairValue"`
-	Bvps                       float64                 `json:"bvps"`
-	Pbv                        float64                 `json:"pbv"`
-	Roa                        float64                 `json:"roa"`
-	Roe                        float64                 `json:"roe"`
-	NPL                        float64                 `json:"npl"`
-	Mktcap                     float64                 `json:"mktCap"`
-	DistributableProfit        float64                 `json:"distributableProfit"`
-	DistibutableProfitPerShare float64                 `json:"distributableProfitPerShare"`
-	PaidUpCapital              float64                 `json:"paidUpCapital"`
-	DividendCapacity           float64                 `json:"dividendCapacity"`
-	RetentionRatio             float64                 `json:"retentionRatio"`
-	Hydro                      HydroKeyMetrics         `json:"hydro"`
-	Hotel                      HotelKeyMetrics         `json:"hotel"`
-	LifeInsurance              LifeInsuranceKeyMetrics `json:"lifeInsurance"`
+	Ticker                     string                     `json:"ticker"`
+	LTP                        float64                    `json:"ltp"`
+	DiversionFromFair          float64                    `json:"divesionFromFair"`
+	PE                         float64                    `json:"pe"`
+	Eps                        float64                    `json:"eps"`
+	FairValue                  float64                    `json:"fairValue"`
+	Bvps                       float64                    `json:"bvps"`
+	Pbv                        float64                    `json:"pbv"`
+	Roa                        float64                    `json:"roa"`
+	Roe                        float64                    `json:"roe"`
+	NPL                        float64                    `json:"npl"`
+	Mktcap                     float64                    `json:"mktCap"`
+	DistributableProfit        float64                    `json:"distributableProfit"`
+	DistibutableProfitPerShare float64                    `json:"distributableProfitPerShare"`
+	PaidUpCapital              float64                    `json:"paidUpCapital"`
+	DividendCapacity           float64                    `json:"dividendCapacity"`
+	RetentionRatio             float64                    `json:"retentionRatio"`
+	Hydro                      HydroKeyMetrics            `json:"hydro"`
+	Hotel                      HotelKeyMetrics            `json:"hotel"`
+	LifeInsurance              LifeInsuranceKeyMetrics    `json:"lifeInsurance"`
+	NonLifeInsurance           NonLifeInsuranceKeyMetrics `json:"nonLifeInsurance"`
+	Manufacturing              ManufacturingKeyMetrics    `json:"manufacturing"`
 }
 
 type HydroKeyMetrics struct {
@@ -55,11 +57,30 @@ type LifeInsuranceKeyMetrics struct {
 	Income             float64 `json:"income"`
 	Expenditure        float64 `json:"expenditure"`
 	NetIncome          float64 `json:"netIncome"`
-	LifeInsurenceFund  float64 `json:"lifeInsurenceFund"`
+	LifeInsuranceFund  float64 `json:"lifeInsuranceFund"`
 	ReserveAndSurplus  float64 `json:"reserveAndSurplus"`
 	TotalRevenue       float64 `json:"totalRevenue"`
 	GrossProfit        float64 `json:"grossProfit"`
 	CatastropheReserve float64 `json:"catastropheReserve"`
+}
+
+type NonLifeInsuranceKeyMetrics struct {
+	Income             float64 `json:"income"`
+	Expenditure        float64 `json:"expenditure"`
+	NetIncome          float64 `json:"netIncome"`
+	InsuranceFund      float64 `json:"InsuranceFund"`
+	ReserveAndSurplus  float64 `json:"reserveAndSurplus"`
+	TotalRevenue       float64 `json:"totalRevenue"`
+	GrossProfit        float64 `json:"grossProfit"`
+	CatastropheReserve float64 `json:"catastropheReserve"`
+}
+
+type ManufacturingKeyMetrics struct {
+	TotalIncome               float64 `json:"totalIncome"`
+	TotalExpenditure          float64 `json:"totalExpenditure"`
+	NetIncome                 float64 `json:"netIncome"`
+	TotalEquityAndLiabilities float64 `json:"totalEquityAndLiabilities"`
+	TotalRevenue              float64 `json:"totalRevenue"`
 }
 
 func (server *Server) GetFundamentalSectorwise(w http.ResponseWriter, r *http.Request) {
@@ -185,7 +206,7 @@ func (server *Server) GetFundamentalSectorwise(w http.ResponseWriter, r *http.Re
 				}
 
 				if len(balancesheet.Message.Data) != 0 {
-					key.LifeInsurance.LifeInsurenceFund = balancesheet.Message.Data[0].Lifeinsurancefund
+					key.LifeInsurance.LifeInsuranceFund = balancesheet.Message.Data[0].Lifeinsurancefund
 					key.LifeInsurance.CatastropheReserve = balancesheet.Message.Data[0].Catastrophereserve
 					key.LifeInsurance.ReserveAndSurplus = balancesheet.Message.Data[0].Reservesurplus
 				}
@@ -194,6 +215,37 @@ func (server *Server) GetFundamentalSectorwise(w http.ResponseWriter, r *http.Re
 					key.LifeInsurance.NetIncome = financial.Message.Data[0].Netincome
 					key.LifeInsurance.GrossProfit = financial.Message.Data[0].Grossprofit
 					key.LifeInsurance.TotalRevenue = financial.Message.Data[0].Totalrevenue
+				}
+			}
+
+			if sector == "Non Life Insurance" {
+				if len(incomeStatement.Message.Data) > 0 {
+					key.NonLifeInsurance.Income = incomeStatement.Message.Data[0].Income
+					key.NonLifeInsurance.Expenditure = incomeStatement.Message.Data[0].Expenses
+				}
+				if len(balancesheet.Message.Data) != 0 {
+					key.NonLifeInsurance.InsuranceFund = balancesheet.Message.Data[0].Insurancefund
+					key.NonLifeInsurance.ReserveAndSurplus = balancesheet.Message.Data[0].Reservesurplus
+					key.NonLifeInsurance.CatastropheReserve = balancesheet.Message.Data[0].Catastrophereserve
+				}
+				if len(financial.Message.Data) != 0 {
+					key.NonLifeInsurance.NetIncome = financial.Message.Data[0].Netincome
+					key.NonLifeInsurance.GrossProfit = financial.Message.Data[0].Grossprofit
+					key.NonLifeInsurance.TotalRevenue = financial.Message.Data[0].Totalrevenue
+				}
+			}
+
+			if sector == "Manufacturing And Processing" {
+				if len(incomeStatement.Message.Data) > 0 {
+					key.Manufacturing.TotalIncome = incomeStatement.Message.Data[0].Totalincome
+					key.Manufacturing.TotalExpenditure = incomeStatement.Message.Data[0].Totalexpenditure
+					key.Manufacturing.NetIncome = incomeStatement.Message.Data[0].Netprofit
+				}
+				if len(balancesheet.Message.Data) != 0 {
+					key.Manufacturing.TotalEquityAndLiabilities = balancesheet.Message.Data[0].Totaleqli
+				}
+				if len(financial.Message.Data) != 0 {
+					key.Manufacturing.TotalRevenue = financial.Message.Data[0].Totalrevenue
 				}
 			}
 
@@ -274,6 +326,25 @@ func GetHeaders(sector string) map[string]string {
 		headers["Q1"] = "Reserve"
 	}
 
+	if sector == "Non Life Insurance" {
+		headers["J1"] = "Income"
+		headers["K1"] = "Expenditure"
+		headers["L1"] = "Net Income"
+		headers["M1"] = "Insurance Fund"
+		headers["N1"] = "Catastrophe Reserve"
+		headers["O1"] = "Total Revenue"
+		headers["P1"] = "Gross Profit"
+		headers["Q1"] = "Reserve"
+	}
+
+	if sector == "Manufacturing And Processing" {
+		headers["J1"] = "Income"
+		headers["K1"] = "Expenditure"
+		headers["L1"] = "Net Profit"
+		headers["M1"] = "Total Revenue"
+		headers["N1"] = "Total Equity"
+	}
+
 	return headers
 }
 
@@ -304,11 +375,28 @@ func GetValues(sector string, data KeyFinancialMetrics, k int) map[string]interf
 		excelVal[fmt.Sprintf("J%d", k+2)] = data.LifeInsurance.Income
 		excelVal[fmt.Sprintf("K%d", k+2)] = data.LifeInsurance.Expenditure
 		excelVal[fmt.Sprintf("L%d", k+2)] = data.LifeInsurance.NetIncome
-		excelVal[fmt.Sprintf("M%d", k+2)] = data.LifeInsurance.LifeInsurenceFund
+		excelVal[fmt.Sprintf("M%d", k+2)] = data.LifeInsurance.LifeInsuranceFund
 		excelVal[fmt.Sprintf("N%d", k+2)] = data.LifeInsurance.CatastropheReserve
 		excelVal[fmt.Sprintf("O%d", k+2)] = data.LifeInsurance.TotalRevenue
 		excelVal[fmt.Sprintf("P%d", k+2)] = data.LifeInsurance.GrossProfit
 		excelVal[fmt.Sprintf("Q%d", k+2)] = data.LifeInsurance.ReserveAndSurplus
+	}
+	if sector == "Non Life Insurance" {
+		excelVal[fmt.Sprintf("J%d", k+2)] = data.NonLifeInsurance.Income
+		excelVal[fmt.Sprintf("K%d", k+2)] = data.NonLifeInsurance.Expenditure
+		excelVal[fmt.Sprintf("L%d", k+2)] = data.NonLifeInsurance.NetIncome
+		excelVal[fmt.Sprintf("M%d", k+2)] = data.NonLifeInsurance.InsuranceFund
+		excelVal[fmt.Sprintf("N%d", k+2)] = data.NonLifeInsurance.CatastropheReserve
+		excelVal[fmt.Sprintf("O%d", k+2)] = data.NonLifeInsurance.TotalRevenue
+		excelVal[fmt.Sprintf("P%d", k+2)] = data.NonLifeInsurance.GrossProfit
+		excelVal[fmt.Sprintf("Q%d", k+2)] = data.NonLifeInsurance.ReserveAndSurplus
+	}
+	if sector == "Manufacturing And Processing" {
+		excelVal[fmt.Sprintf("J%d", k+2)] = data.Manufacturing.TotalIncome
+		excelVal[fmt.Sprintf("K%d", k+2)] = data.Manufacturing.TotalExpenditure
+		excelVal[fmt.Sprintf("L%d", k+2)] = data.Manufacturing.NetIncome
+		excelVal[fmt.Sprintf("M%d", k+2)] = data.Manufacturing.TotalRevenue
+		excelVal[fmt.Sprintf("N%d", k+2)] = data.Manufacturing.TotalEquityAndLiabilities
 	}
 
 	return excelVal
