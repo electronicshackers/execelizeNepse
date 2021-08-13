d361e40419f589034372bcfeb53268e899087fa3
Kiran Adhikari <kiran.adhikari@innovatetech.co>
2021-08-13 17:09:42 +0545

    Some Map's for testing

--- api/controllers/floorsheet.go
+++ api/controllers/floorsheet.go
@@ -1,12 +1,19 @@
 package controllers
 
 import (
-	"fmt"
 	responses "nepse-backend/api/response"
 	"nepse-backend/nepse"
 	"nepse-backend/nepse/neweb"
 	"net/http"
 )
+
+type FloorsheetResult struct {
+	Ticker            string             `json:"ticker"`
+	BuyerQuantityMap  map[string]int     `json:"buyerQuantityMap"`
+	BuyerTurnOverMap  map[string]float64 `json:"buyerTurnOverMap"`
+	SellerQuantityMap map[string]int     `json:"sellerQuantityMap"`
+	SellerTurnOverMap map[string]float64 `json:"sellerTurnOverMap"`
+}
 
 func (s *Server) GetFloorsheet(w http.ResponseWriter, r *http.Request) {
 	ticker := r.URL.Query().Get("ticker")
@@ -14,7 +21,6 @@ func (s *Server) GetFloorsheet(w http.ResponseWriter, r *http.Request) {
 
 	randomId := r.URL.Query().Get("id")
 
-	fmt.Println("start", start, "randomId", randomId)
 	var nepseBeta nepse.NepseInterface
 
 	nepseBeta, err := neweb.Neweb()
@@ -53,5 +59,28 @@ func (s *Server) GetFloorsheet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	responses.JSON(w, http.StatusOK, floorsheetInfoAgg)
+	var result FloorsheetResult
+
+	result.BuyerQuantityMap = make(map[string]int)
+	result.SellerQuantityMap = make(map[string]int)
+	result.BuyerTurnOverMap = make(map[string]float64)
+	result.SellerTurnOverMap = make(map[string]float64)
+
+	for _, sheetData := range floorsheetInfoAgg.Floorsheets.Content {
+		if sheetData.Buyermemberid != "" {
+			result.BuyerQuantityMap[sheetData.Buyermemberid] += sheetData.Contractquantity
+		}
+		if sheetData.Sellermemberid != "" {
+			result.SellerQuantityMap[sheetData.Sellermemberid] += sheetData.Contractquantity
+		}
+		if sheetData.Buyermemberid != "" {
+			result.BuyerTurnOverMap[sheetData.Buyermemberid] += sheetData.Contractamount
+		}
+		if sheetData.Sellermemberid != "" {
+			result.SellerTurnOverMap[sheetData.Sellermemberid] += sheetData.Contractamount
+		}
+	}
+	result.Ticker = ticker
+
+	responses.JSON(w, http.StatusOK, result)
 }
